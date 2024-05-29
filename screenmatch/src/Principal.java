90a37ced18dff19216e4fd45c9f644da5d93033f
Thales Joazeiro <thalesjoazeiro@gmail.com>
2024-05-29 10:45:00 -0300

    Estudando Java

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,27 +1,49 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
 
-        meuFilme.setNome("The Matrix");
-        meuFilme.setAnoDeLancamento(1999);
-        meuFilme.setDuracaoEmMinutos(135);
-        meuFilme.setIncluidoNoPlano(true);
+        Filme favorito = new Filme();
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
 
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(9);
-        meuFilme.avalia(8);
-        meuFilme.avalia(9);
+        Filme outro = new Filme();
+        outro.setNome("John Wick");
+        outro.setAnoDeLancamento(2014);
+        outro.setDuracaoEmMinutos(101);
+        outro.setIncluidoNoPlano(true);
 
-        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        Serie serie = new Serie();
+        serie.setNome("La Casa de Papel");
+        serie.setAnoDeLancamento(2017);
+        serie.setIncluidoNoPlano(true);
+        serie.setAtiva(true);
+        serie.setTemporadas(5);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(45);
 
-        System.out.println("Média de avaliações do filme: " + meuFilme.pegaMedia());
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outro);
+        calculadora.inclui(serie);
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
+        System.out.println("Tempo total: " +calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(favorito);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(serie);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 
 }
